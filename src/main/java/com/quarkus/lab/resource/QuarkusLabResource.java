b2f58b48ef9e5aac3b03f91ea7f7237d0bebe375
joao-nzuzi <joaonzuzi21@gmail.com>
2025-07-17 13:03:20 +0200

    Initial commit

---
+++ src/main/java/com/quarkus/lab/resource/QuarkusLabResource.java
@@ -0,0 +1,16 @@
+package com.quarkus.lab.resource;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class QuarkusLabResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello from Quarkus REST";
+    }
+}
